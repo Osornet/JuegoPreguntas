545f2944cc5432a4f0f586193d64197036dc498b
JAVIER <JAVIER@JAVIER-PC>
2018-05-21 18:06:15 -0500

    Agregue Libreria SweetAlert

--- src/java/test/PruebaJavier.java
+++ src/java/test/PruebaJavier.java
@@ -5,7 +5,6 @@
  */
 package test;
 
-import com.juegoPreguntas.modelo.persistencia.daos.hibernate.PreguntaDAOImp;
 import com.juegoPreguntas.modelo.persistencia.daos.hibernate.RespuestaDAOImp;
 import com.juegoPreguntas.modelo.pojo.Pregunta;
 import com.juegoPreguntas.modelo.pojo.Respuesta;
@@ -14,19 +13,17 @@ import com.juegoPreguntas.modelo.pojo.Respuesta;
  *
  * @author JAVIER
  */
-public class PruebaJavier {
-    
+public class PruebaJavier {    
+
     public static void main(String[] args) {
 
-        RespuestaDAOImp daores = new RespuestaDAOImp();
-        Respuesta r = new Respuesta(); 
-        Pregunta pregunta = new Pregunta();
-        pregunta.setId(1);
-        r.setDescripcion("Respuesta correcta");
-        r.setCorrecta((byte)1);
+        
+        RespuestaDAOImp daores = new RespuestaDAOImp(); Respuesta r = new
+        Respuesta(); Pregunta pregunta = new Pregunta(); pregunta.setId(1);
+        r.setDescripcion("Respuesta correcta"); r.setCorrecta((byte)1);
         r.setPregunta(pregunta);
         daores.insertar(r);
-        
-
-    }
+         
+       
+    }  
 }
